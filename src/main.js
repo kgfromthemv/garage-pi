a27a4665e3f79fc2c8c00f6d37617b8d1fec294e
Kyle Grieder <kylegrieder@kyles-mbp.lan>
2018-05-21 20:33:35 -0600

    added camera module

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { Accessory, Service, Characteristic, uuid } from 'hap-nodejs';
 import storage from 'node-persist';
 import doorController from './door';
 import config from '../config.json';
+import Camera from './camera';
 const debug = require('debug')('controller:main');
 
 storage.initSync();
@@ -11,9 +12,20 @@ debug(`accessory username: ${config.door.accessory.username}`);
 debug(`accessory pincode: ${config.door.accessory.pincode}`);
 debug(`accessory port: ${config.door.accessory.port}`);
 
+debug(`camera name: ${config.camera.accessory.name}`);
+debug(`camera username: ${config.camera.accessory.username}`);
+debug(`camera pincode: ${config.camera.accessory.pincode}`);
+debug(`camera port: ${config.camera.accessory.port}`);
+
 async function controller() {
   const doorUUID = uuid.generate(`hap-nodejs:accessories:${config.door.accessory.name}`);
   const doorAccessory = exports.accessory = new Accessory(config.door.accessory.name, doorUUID);
+
+  const cameraSource = new Camera();
+
+  const cameraUUID = uuid.generate(`hap-nodejs:accessories:${config.camera.accessory.name}`);
+  const cameraAccessory = exports.camera = new Accessory(config.camera.accessory.name, cameraUUID);
+
 
   // Door Accessory
 
@@ -93,6 +105,14 @@ async function controller() {
       }
     });
 
+  // Camera Accessory
+
+  cameraAccessory.configureCameraSource(cameraSource);
+
+  cameraAccessory.identify, (paired, callback) => {
+    callback();
+  }
+
   debug('publish door accessory');
   doorAccessory.publish({
     port: config.door.accessory.port,
@@ -101,6 +121,14 @@ async function controller() {
     category: Accessory.Categories.GARAGE_DOOR_OPENER,
   });
   setCurrentDoorState(doorAccessory, false);
+
+  debug('publish camera accessory');
+  cameraAccessory.publish({
+    port: config.camera.accessory.port,
+    username: config.camera.accessory.username,
+    pincode: config.camera.accessory.pincode,
+    category: Accessory.Categories.CAMERA,
+  });
 }
 
 async function setCurrentDoorState(doorAccessory, now) {
