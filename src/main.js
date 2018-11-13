060b3d24e8efd9f32806039d5a1ff9696e7d0ee2
Kyle Grieder <kylegrieder@kyles-mbp.lan>
2018-11-12 18:53:07 -0700

    added code to check door state

--- src/main.js
+++ src/main.js
@@ -107,6 +107,11 @@ async function controller() {
             callback(err, Characteristic.CurrentDoorState.CLOSED);
         }
 });
+  let curDoorState = await doorState()
+  doorAccessory
+    .getService(Service.GarageDoorOpener)
+    .getCharacteristic(Characteristic.CurrentDoorState)
+    .setValue(curDoorState)
 
     // Camera Accessory
 
